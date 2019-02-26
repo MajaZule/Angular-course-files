4c95941a699be4d20f8996843ba80b74be292492
MajaZule <44669001+MajaZule@users.noreply.github.com>
2019-02-26 15:05:30 +0100

    Component styles

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,7 +5,13 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-server',
-	templateUrl: './server.component.html'
+	templateUrl: '<app-server></app-server><app-server></app-server>',
+	styleUrl: ['./app.component.css'],
+	styles: [`
+		h3 {
+			color: red;
+		}
+	`]
 })
 export class ServerComponent {
 	
